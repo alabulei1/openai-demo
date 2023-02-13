805a9aeb80b3fc42f400b1b5321de7b9403d81c4
alabulei1 <vivian.xiage@gmail.com>
2023-02-13 17:51:33 +0800

    Update openapi.rs

--- src/openapi.rs
+++ src/openapi.rs
@@ -3,14 +3,14 @@ use slack_flows::{listen_to_channel, send_message_to_channel};
 
 #[no_mangle]
 pub fn run() {
-    listen_to_channel("myworkspace", "mychannel", |sm| {
+    listen_to_channel("WasmEdge", "general", |sm| {
         let cr = CompletionRequest {
             prompt: sm.text,
             ..Default::default()
         };
-        let r = create_completion("myaccount", cr);
+        let r = create_completion("openapi", cr);
         r.iter().for_each(|c| {
-            send_message_to_channel("myworkspace", "mychannel", c.to_string());
+            send_message_to_channel("WasmEdge", "general", c.to_string());
         });
     });
 }
